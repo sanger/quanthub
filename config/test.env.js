c33dc307e348c1c88cc8fb360c54f677415d4c2b
Luca Da Rin Fioretto <ldrf@sanger.ac.uk>
2018-02-06 16:53:36 +0000

    Trying headless chrome

--- config/test.env.js
+++ config/test.env.js
@@ -3,5 +3,16 @@ const merge = require('webpack-merge')
 const devEnv = require('./dev.env')
 
 module.exports = merge(devEnv, {
-  NODE_ENV: '"testing"'
+  NODE_ENV: '"testing"',
+  config.set({
+      browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessNoSandbox'],
+
+      // you can define custom flags
+      customLaunchers: {
+        ChromeHeadlessNoSandbox: {
+          base: 'ChromeHeadless',
+          flags: ['--no-sandbox']
+        }
+      }
+    })
 })
