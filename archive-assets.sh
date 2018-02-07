620fe8fa62ae51711ac19a4c73eea7ae36c3b718
Luca Da Rin Fioretto <ldrf@sanger.ac.uk>
2018-02-07 16:20:45 +0000

    Moved build command to the deployment phase

--- archive-assets.sh
+++ archive-assets.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -ev
 
+# Building assets
+npm run build
+
 # Storing revision hash
-git rev-parse HEAD > dist/REVISION
+git rev-parse HEAD > $2/REVISION
 
 # Creating tar.gz
-tar -zcvf $1 -C dist .
+tar -zcvf $1 -C $2 .
