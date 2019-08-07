1b1c4f51bcfa2c967300fbc67d5afa92369778b3
Andrew Sparkes <as28@sanger.ac.uk>
2019-08-07 12:34:04 +0100

    Added error handling to Upload and clear local storage button

--- src/components/QuantFile.vue
+++ src/components/QuantFile.vue
@@ -59,7 +59,7 @@ export default {
         return this.metadata[this.quantType.metadata.idColumn].split('-')[0]
       } else {
         return this.parsedFilename
-      } 
+      }
     },
     parsedFilename () {
       return this.filename.split('_')[1].split('-')[0]
@@ -67,20 +67,22 @@ export default {
   },
   methods: {
     upload (file) {
-      // TODO: need to handle errors.
-      // can't seem to get onerror to work.
       // A new file reader object gets the raw data.
       // The file is parsed by the quant type options and
       // a factory is used to ensure standardisation of the data
       // for when it is added to the grid.
-      return new Promise((resolve, ) => {
+      return new Promise((resolve, reject) => {
         const reader = new FileReader()
         reader.onload = () => {
           // \r\r\n is a non standard windows line ending which causes all sorts of problems.
           // TODO: move it out into a constant.
-          this.raw = reader.result.replace(/\r\r\n/g, '\n')
-          this.grid = new this.GridCmp({propsData: {quantType: this.quant}})
-          this.grid.addAll(parse(this.raw, this.quantType.parse).map(cell => new this.quantType.WellFactory(cell, WellMap).json))
+          try {
+            this.raw = reader.result.replace(/\r\r\n/g, '\n')
+            this.grid = new this.GridCmp({propsData: {quantType: this.quant}})
+            this.grid.addAll(parse(this.raw, this.quantType.parse).map(cell => new this.quantType.WellFactory(cell, WellMap).json))
+          } catch(error) {
+            reject(`Failed to parse: ${error.message}`)
+          }
           resolve('File successfully uploaded')
         }
         reader.readAsText(file)
