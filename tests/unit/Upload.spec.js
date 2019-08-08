3c67c4083da64dda4a622e6704413c6e87cf3612
Andrew Sparkes <as28@sanger.ac.uk>
2019-08-08 11:27:13 +0100

    Fix to config to increase metadata lines for new reader software, and fix to upload unit test to allow for use of Alert component

--- tests/unit/Upload.spec.js
+++ tests/unit/Upload.spec.js
@@ -1,12 +1,12 @@
 import Upload from '@/components/Upload'
-import { mount } from '@vue/test-utils'
+import { mount, localVue } from './testHelper'
 
 describe('Upload.vue', () => {
 
   let cmp, upload
 
   beforeEach(() => {
-    cmp = mount(Upload, { } )
+    cmp = mount(Upload, { localVue })
     upload = cmp.vm
   })
 
