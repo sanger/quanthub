082377ead172d9df8261103f209aae9b20985861
James Glover <james.glover@sanger.ac.uk>
2021-10-13 16:54:05 +0100

    Refactor well factories to return simple objects
    We were only using the json method of these classes, so may
    as well just simplify things down and return a bas js object.
    
    Meanwhile, we've massively simplified the API, so can simplify
    the tests.
    
    Can probably do another refactor pass, but tests green at the
    moment.

--- src/QuantTypeWellFactories.js
+++ src/QuantTypeWellFactories.js
@@ -1,119 +1,60 @@
-// TODO: abstract out common behaviour, maybe turn into a DSL.
-class PlateReader {
-  constructor(cell = {}) {
-    this.row = cell.row
-    this.column = cell.column
-    this.content = cell.content
-    this.id = cell.id
-    this.concentration = cell.concentration
-  }
-  get type() {
-    return this.content.split(' ')[0] || 'Empty'
-  }
-  get json() {
-    return {
-      row: this.row,
-      column: this.column,
-      type: this.type,
-      id: this.id,
-      concentration: this.concentration,
-    }
+const PlateReader = ({ row, content, column, id, concentration }) => {
+  const type = content.split(' ')[0] || 'Empty'
+
+  return {
+    row,
+    column,
+    type,
+    id,
+    concentration,
   }
 }
 
-class QPCR10ul {
-  constructor(cell = {}) {
-    this.include = cell.include
-    this.color = cell.color
-    this.pos = cell.pos
-    this.name = cell.name
-    this.cp = cell.cp
-    this.concentration = Number(cell.concentration)
-    this.standard = cell.standard
-    this.status = cell.status
-  }
-  get row() {
-    return this.pos.match(/[a-zA-Z]+/g).toString()
-  }
-  get column() {
-    return this.pos.match(/[0-9]+/g).toString()
-  }
-  isSample() {
-    return /^[A-P]\d{1,2}$/.test(this.name)
-  }
-  get type() {
-    return this.isSample() ? 'Sample' : 'Standard'
-  }
-  get id() {
-    return this.isSample() ? this.name : ''
-  }
-  get json() {
-    return {
-      row: this.row,
-      column: this.column,
-      type: this.type,
-      id: this.id,
-      concentration: this.concentration,
-    }
+const QPCR10ul = ({ pos, name, concentration }) => {
+  const row = pos.match(/[a-zA-Z]+/g).toString()
+  const column = pos.match(/[0-9]+/g).toString()
+  const isSample = /^[A-P]\d{1,2}$/.test(name)
+  const type = isSample ? 'Sample' : 'Standard'
+  const id = isSample ? name : ''
+
+  return {
+    row,
+    column,
+    type,
+    id,
+    concentration: Number(concentration),
   }
 }
 
-class QPCR5ul {
-  constructor(cell = {}, wellMap = {}) {
-    this.well = cell.well
-    this.copyNumber = cell.copyNumber
-    this.replicateError = cell.replicateError
-    this.totalError = cell.totalError
-    this.shapeZScore = cell.shapeZScore
-    this.comments = cell.comments
-    this.wellMap = wellMap
-  }
-  get row() {
-    return this.well.match(/[a-zA-Z]+/g).toString()
-  }
-  get column() {
-    return this.well.match(/[0-9]+/g).toString()
-  }
-  get id() {
-    return this.wellMap[this.well]
-  }
-  get type() {
-    return this.id ? 'Sample' : 'Empty'
-  }
-  get json() {
-    return {
-      row: this.row,
-      column: this.column,
-      type: this.type,
-      id: this.id,
-      concentration: Number(this.copyNumber),
-    }
+const QPCR5ul = ({ well, copyNumber }, wellMap = {}) => {
+  const row = well.match(/[a-zA-Z]+/g).toString()
+  const column = well.match(/[0-9]+/g).toString()
+  const id = wellMap[well]
+  const type = id ? 'Sample' : 'Empty'
+
+  return {
+    row,
+    column,
+    type,
+    id,
+    concentration: Number(copyNumber),
   }
 }
 
-class TubeTapeStation {
-  constructor(cell = {}) {
-    this.well = cell.wellId
-    this.id = cell.sampleDescription
-    this.concentration = cell.regionMolarity
-  }
-  get row() {
-    return this.well.match(/[a-zA-Z]+/g).toString()
-  }
-  get column() {
-    return this.well.match(/[0-9]+/g).toString()
-  }
-  get type() {
-    return 'Sample'
-  }
-  get json() {
-    return {
-      row: this.row,
-      column: this.column,
-      type: this.type,
-      id: this.id,
-      concentration: this.concentration,
-    }
+const TubeTapeStation = ({ wellId, sampleDescription, regionMolarity }) => {
+  const well = wellId
+  const id = sampleDescription
+  const concentration = regionMolarity
+  const row = well.match(/[a-zA-Z]+/g).toString()
+  const column = well.match(/[0-9]+/g).toString()
+  const type = 'Sample'
+
+  return {
+    row,
+    column,
+    type,
+    id,
+    concentration,
   }
 }
 
--- tests/unit/QuantTypeWellFactories.spec.js
+++ tests/unit/QuantTypeWellFactories.spec.js
@@ -1,150 +1,64 @@
 import * as WellFactories from '@/QuantTypeWellFactories'
 
 describe('QuantTypeWellFactories.js', () => {
-  let options, well
-
   describe('PlateReader Factory', () => {
-    beforeEach(() => {
-      options = {
+    describe('Sample well', () => {
+      const options = {
         row: 'A',
         column: '1',
         content: 'Sample X1',
         id: 'A1',
         concentration: 1.345,
       }
-      well = new WellFactories.PlateReader(options)
-    })
 
-    it('must have a row', () => {
-      expect(well.row).toEqual(options.row)
-    })
-
-    it('must have a column', () => {
-      expect(well.column).toEqual(options.column)
-    })
-
-    it('must have some content', () => {
-      expect(well.content).toEqual(options.content)
-    })
-
-    it('must have an id', () => {
-      expect(well.column).toEqual(options.column)
-    })
-
-    it('must have a concentration', () => {
-      expect(well.concentration).toEqual(options.concentration)
-    })
-
-    it('must have a type', () => {
-      expect(well.type).toEqual('Sample')
-    })
-
-    it('produces some json', () => {
-      expect(well.json).toEqual({
-        row: 'A',
-        column: '1',
-        type: 'Sample',
-        id: 'A1',
-        concentration: 1.345,
+      it('produces some json', () => {
+        expect(WellFactories.PlateReader(options)).toEqual({
+          row: 'A',
+          column: '1',
+          type: 'Sample',
+          id: 'A1',
+          concentration: 1.345,
+        })
       })
     })
 
     describe('Empty well', () => {
-      beforeEach(() => {
-        well = new WellFactories.PlateReader({
+      const options = {
+        row: '',
+        column: '',
+        content: '',
+        type: '',
+        id: '',
+        concentration: '',
+      }
+
+      it('produces some json', () => {
+        expect(WellFactories.PlateReader(options)).toEqual({
           row: '',
           column: '',
-          content: '',
-          type: '',
+          type: 'Empty',
           id: '',
           concentration: '',
         })
-      })
-
-      it('must have a type', () => {
-        expect(well.type).toEqual('Empty')
-      })
-
-      it('will be empty apart from the type', () => {
-        expect(well.row).toEqual('')
-        expect(well.column).toEqual('')
-        expect(well.content).toEqual('')
-        expect(well.id).toEqual('')
-        expect(well.concentration).toEqual('')
       })
     })
   })
 
   describe('QPCRWellFactory - 10ul', () => {
-    describe('sample', () => {
-      beforeEach(() => {
-        options = {
-          include: 'TRUE',
-          color: '128',
-          pos: 'N1',
-          name: 'A1',
-          cp: '24.21',
-          concentration: '5.61E+01',
-          standard: '0.0002',
-          status: 'red',
-        }
-        well = new WellFactories.QPCR10ul(options)
-      })
-
-      it('must have an include', () => {
-        expect(well.include).toEqual(options.include)
-      })
-
-      it('must have a color', () => {
-        expect(well.color).toEqual(options.color)
-      })
-
-      it('must have a pos', () => {
-        expect(well.pos).toEqual(options.pos)
-      })
-
-      it('must have an name', () => {
-        expect(well.name).toEqual(options.name)
-      })
-
-      it('must have a cp', () => {
-        expect(well.cp).toEqual(options.cp)
-      })
-
-      it('must have a concentration', () => {
-        expect(well.concentration).toEqual(56.1)
-      })
-
-      it('must have a standard', () => {
-        expect(well.standard).toEqual(options.standard)
-      })
-
-      it('must have a status', () => {
-        expect(well.status).toEqual(options.status)
-      })
-
-      it('must have a row', () => {
-        expect(well.row).toEqual('N')
-      })
-
-      it('must have a column', () => {
-        expect(well.column).toEqual('1')
-      })
-
-      it('will have an id', () => {
-        expect(well.id).toEqual('A1')
-      })
-
-      it('will have a type', () => {
-        expect(well.type).toEqual('Sample')
-      })
-
-      it('must be a sample', () => {
-        expect(well.isSample()).toBeTruthy()
-      })
+    describe('Sample', () => {
+      const options = {
+        include: 'TRUE',
+        color: '128',
+        pos: 'N1',
+        name: 'A1',
+        cp: '24.21',
+        concentration: '5.61E+01',
+        standard: '0.0002',
+        status: 'red',
+      }
 
       it('produces some json', () => {
-        expect(well.json).toEqual({
+        expect(WellFactories.QPCR10ul(options)).toEqual({
           row: 'N',
           column: '1',
           type: 'Sample',
@@ -155,126 +69,67 @@ describe('QuantTypeWellFactories.js', () => {
     })
 
     describe('bugfix - column is 10 or above', () => {
-      beforeEach(() => {
-        options = {
-          include: 'TRUE',
-          color: '128',
-          pos: 'D23',
-          name: 'B6',
-          cp: '24.21',
-          concentration: '5.61E+01',
-          standard: '0.0002',
-          status: 'red',
-        }
-        well = new WellFactories.QPCR10ul(options)
-      })
+      const options = {
+        include: 'TRUE',
+        color: '128',
+        pos: 'D23',
+        name: 'B6',
+        cp: '24.21',
+        concentration: '5.61E+01',
+        standard: '0.0002',
+        status: 'red',
+      }
 
-      it('will have the correct row', () => {
-        expect(well.row).toEqual('D')
-      })
-
-      it('will have the correct column', () => {
-        expect(well.column).toEqual('23')
+      it('has the correct row and column', () => {
+        expect(WellFactories.QPCR10ul(options)).toEqual({
+          row: 'D',
+          column: '23',
+          type: 'Sample',
+          id: 'B6',
+          concentration: 56.1,
+        })
       })
     })
 
     describe('standard', () => {
-      beforeEach(() => {
-        options = {
-          include: 'TRUE',
-          color: '128',
-          pos: 'N1',
-          name: '0.0002pM',
-          cp: '24.21',
-          concentration: '2.66E-04',
-          standard: '0.0002',
-          status: 'red',
-        }
-        well = new WellFactories.QPCR10ul(options)
-      })
+      const options = {
+        include: 'TRUE',
+        color: '128',
+        pos: 'N1',
+        name: '0.0002pM',
+        cp: '24.21',
+        concentration: '2.66E-04',
+        standard: '0.0002',
+        status: 'red',
+      }
 
-      it('will have a name', () => {
-        expect(well.name).toEqual('0.0002pM')
-      })
-
-      it('will not have an id', () => {
-        expect(well.id).toEqual('')
-      })
-
-      it('will have a type', () => {
-        expect(well.type).toEqual('Standard')
-      })
-
-      it('must not be a sample', () => {
-        expect(well.isSample()).toBeFalsy()
-      })
-
-      it('will have a concentration', () => {
-        expect(well.concentration).toEqual(0.000266)
+      it('produces some json', () => {
+        expect(WellFactories.QPCR10ul(options)).toEqual({
+          row: 'N',
+          column: '1',
+          type: 'Standard',
+          id: '',
+          concentration: 0.000266,
+        })
       })
     })
   })
 
   describe('QPCRWellFactory - 5ul', () => {
-    let wellMap
-
     describe('sample', () => {
-      beforeEach(() => {
-        wellMap = { A1: 'Z234', A2: 'X456' }
-        options = {
-          well: 'A1',
-          copyNumber: '48909000',
-          replicateError: '2445450',
-          totalError: '10082849',
-          shapeZScore: '0.695615245',
-          comments:
-            'This well was disregarded due to abnormally low background fluorescence.',
-        }
-        well = new WellFactories.QPCR5ul(options, wellMap)
-      })
-
-      it('must have a well', () => {
-        expect(well.well).toEqual(options.well)
-      })
-
-      it('must have a copy number', () => {
-        expect(well.copyNumber).toEqual(options.copyNumber)
-      })
-
-      it('must have a replicate error', () => {
-        expect(well.replicateError).toEqual(options.replicateError)
-      })
-
-      it('must have a total error', () => {
-        expect(well.totalError).toEqual(options.totalError)
-      })
-
-      it('must have a shape Z score', () => {
-        expect(well.shapeZScore).toEqual(options.shapeZScore)
-      })
-
-      it('must have a comments', () => {
-        expect(well.comments).toEqual(options.comments)
-      })
-
-      it('must have a row', () => {
-        expect(well.row).toEqual('A')
-      })
-
-      it('must have a column', () => {
-        expect(well.column).toEqual('1')
-      })
-
-      it('must have an id', () => {
-        expect(well.id).toEqual('Z234')
-      })
-
-      it('must have a type', () => {
-        expect(well.type).toEqual('Sample')
-      })
+      const options = {
+        well: 'A1',
+        copyNumber: '48909000',
+        replicateError: '2445450',
+        totalError: '10082849',
+        shapeZScore: '0.695615245',
+        comments:
+          'This well was disregarded due to abnormally low background fluorescence.',
+      }
+      const wellMap = { A1: 'Z234', A2: 'X456' }
 
       it('produces some json', () => {
-        expect(well.json).toEqual({
+        expect(WellFactories.QPCR5ul(options, wellMap)).toEqual({
           row: 'A',
           column: '1',
           type: 'Sample',
@@ -285,64 +140,39 @@ describe('QuantTypeWellFactories.js', () => {
     })
 
     describe('Empty', () => {
-      beforeEach(() => {
-        well = new WellFactories.QPCR5ul({
-          well: 'Z1',
-          copyNumber: '123',
-          replicateError: '456',
-          totalError: '789',
-          shapeZScore: '0.00',
-          comments: '',
-        })
-      })
+      const options = {
+        well: 'Z1',
+        copyNumber: '123',
+        replicateError: '456',
+        totalError: '789',
+        shapeZScore: '0.00',
+        comments: '',
+      }
 
-      it('must have a type', () => {
-        expect(well.type).toEqual('Empty')
+      it('must have a type of Empty', () => {
+        expect(WellFactories.QPCR5ul(options).type).toEqual('Empty')
       })
     })
   })
 
   describe('TubeTapeStation Factory', () => {
-    beforeEach(() => {
-      // I'm including all the columns from the file here, although in practice
-      // we only rely on an handful.
-      options = {
-        fileName: 'DNXXXXXX - 2019-11-05 - 16.32.56.D5001',
-        wellId: 'A1',
-        sampleDescription: 'NT12345',
-        from: '100',
-        to: '1000',
-        averageSize: '456',
-        conc: '128',
-        regionMolarity: '502',
-        percentageOfTotal: '1.345',
-        regionComment: '',
-      }
-      well = new WellFactories.TubeTapeStation(options)
-    })
-
-    it('must have a row', () => {
-      expect(well.row).toEqual('A')
-    })
-
-    it('must have a column', () => {
-      expect(well.column).toEqual('1')
-    })
-
-    it('must have an id', () => {
-      expect(well.id).toEqual(options.sampleDescription)
-    })
-
-    it('must have a concentration', () => {
-      expect(well.concentration).toEqual(options.regionMolarity)
-    })
-
-    it('must have a type', () => {
-      expect(well.type).toEqual('Sample')
-    })
+    // I'm including all the columns from the file here, although in practice
+    // we only rely on an handful.
+    const options = {
+      fileName: 'DNXXXXXX - 2019-11-05 - 16.32.56.D5001',
+      wellId: 'A1',
+      sampleDescription: 'NT12345',
+      from: '100',
+      to: '1000',
+      averageSize: '456',
+      conc: '128',
+      regionMolarity: '502',
+      percentageOfTotal: '1.345',
+      regionComment: '',
+    }
 
     it('produces some json', () => {
-      expect(well.json).toEqual({
+      expect(WellFactories.TubeTapeStation(options)).toEqual({
         row: 'A',
         column: '1',
         type: 'Sample',
