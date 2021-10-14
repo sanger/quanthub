1b8cfd0842d9616b8f51f6bb4677b8fa6a9151f5
James Glover <james.glover@sanger.ac.uk>
2021-10-14 09:13:58 +0100

    Add documentations

--- src/QuantTypeWellFactories.js
+++ src/QuantTypeWellFactories.js
@@ -1,20 +1,72 @@
+/**
+ * Indicates a well containing a sample which will be subject to QC
+ * Will use: src/components/wells/Sample.vue
+ * @constant {string} */
 const SAMPLE_TYPE = 'Sample'
+
+/**
+ * Indicates a well containing a standard, used to calibrate the curve
+ * Will use: src/components/wells/Standard.vue
+ * @constant {string} */
 const STANDARD_TYPE = 'Standard'
+
+/**
+ * Indicates an empty well
+ * Will use: src/components/wells/Empty.vue
+ * @constant {string} */
 const EMPTY_TYPE = 'Empty'
 
-const splitWellName = (name) => {
-  return {
-    row: name.match(/[a-zA-Z]+/g).toString(),
-    column: name.match(/[0-9]+/g).toString(),
-  }
-}
+/**
+ * A location
+ * @typedef {{column: string, row: string}} WellLocation
+ */
 
-const PlateReader = ({ row, content, column, id, concentration }) => {
+/**
+ * Splits a well name (eg. A12) into its column and row components
+ *
+ * @param {string} name - The name of the well (eg. 'A12')
+ * @return {WellLocation} The {@link WellLocation} of the well (eg. { row: 'A', column: '1' })
+ */
+const splitWellName = (name) =>
+  name.match(/(?<row>[a-zA-Z]+)(?<column>\d+)/).groups
+
+/**
+ * Collects the information extracted for a well
+ *
+ * @typedef {Object} Well
+ * @property {string} row - The well's row (eg. 'A')
+ * @property {string} column - The well's column (eg. '1')
+ * @property {string} type - The partial to yse for the well (eg. 'Sample')
+ * @property {string} id - Identifies the original well or tube this well maps back to
+ * @property {number} concentration - The qc reading for the well
+ */
+
+/**
+ * Processes the results of parsing a Plate reader CSV and returns a {@link Well}
+ *
+ * @param {Object} obj - Object extracted from a CSV row.
+ * @param {string} obj.row - The well's row (eg. 'A')
+ * @param {string} obj.column - The well's column (eg. '1')
+ * @param {string} obj.content - Contents of the Content column (used to work out
+ *                              if we have a control or sample)
+ * @param {string} obj.id - Identifies the original well or tube this well maps back to
+ * @param {number} obj.concentration - The qc reading for the well
+ * @return {Well} The {@link Well} extracted from the csv data
+ */
+const PlateReader = ({ row, column, content, id, concentration }) => {
   const type = content.split(' ')[0] || EMPTY_TYPE
 
   return { row, column, type, id, concentration }
 }
 
+/**
+ * Processes the results of parsing a 10ul QPCR CSV and returns a {@link Well}
+ *
+ * @param {Object} obj - Object extracted from a CSV row.
+ * @param {string} obj.name - The well's name (eg. 'A1')
+ * @param {string} obj.concentration - The qc reading for the well
+ * @return {Well} The {@link Well} extracted from the csv data
+ */
 const QPCR10ul = ({ pos, name, concentration }) => {
   const isSample = /^[A-P]\d{1,2}$/.test(name)
   const type = isSample ? SAMPLE_TYPE : STANDARD_TYPE
@@ -28,6 +80,16 @@ const QPCR10ul = ({ pos, name, concentration }) => {
   }
 }
 
+/**
+ * Processes the results of parsing a 5ul QPCR CSV and returns a {@link Well}
+ *
+ * @param {Object} obj - Object extracted from a CSV row.
+ * @param {string} obj.well - The well's name (eg. 'A1')
+ * @param {string} obj.copyNumber - The qc reading for the well
+ * @param {Object} wellMap - Object mapping wells on the assay plate back to the
+ *                           original
+ * @return {Well} The {@link Well} extracted from the csv data
+ */
 const QPCR5ul = ({ well, copyNumber }, wellMap = {}) => {
   const id = wellMap[well]
   const type = id ? SAMPLE_TYPE : EMPTY_TYPE
@@ -40,6 +102,16 @@ const QPCR5ul = ({ well, copyNumber }, wellMap = {}) => {
   }
 }
 
+/**
+ * Processes the results of parsing a TapeStation CSV and returns a {@link Well}
+ *
+ * @param {Object} obj - Object extracted from a CSV row.
+ * @param {string} obj.wellId - The well's name (eg. 'A1')
+ * @param {string} obj.sampleDescription - The barcode of the tube corresponding
+ *                                         to the well
+ * @param {string} obj.regionMolarity - The qc reading for the well
+ * @return {Well} The {@link Well} extracted from the csv data
+ */
 const TubeTapeStation = ({
   wellId,
   sampleDescription: id,
