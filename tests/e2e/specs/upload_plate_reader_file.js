b855b490f6b49cd51c3cf0bf7029e225227911b9
Stephen Inglis <519327+stevieing@users.noreply.github.com>
2021-10-15 11:31:05 +0100

    Added e2e tests for plate reader and qPCR file uploads.

---
+++ tests/e2e/specs/upload_plate_reader_file.js
@@ -0,0 +1,18 @@
+describe('Plate Reader', () => {
+  it('upload file, check plate is stored and remove it from local storage', () => {
+    cy.visit('/')
+    cy.get('input[type="file"]').as('fileInput').attachFile('plate_reader.csv')
+    cy.get('select[id="quant-type"]').select('libraryPlateReader')
+    cy.contains('button', 'Upload').click()
+    cy.get('.row > h3').contains('DN1234567')
+    cy.get('td').should('have.length', 384)
+
+    cy.visit('#/plates')
+    cy.contains('DN1234567').click()
+    cy.get('.row > h3').contains('DN1234567')
+
+    cy.visit('#/plates')
+    cy.contains('button', 'Clear Local Storage').click()
+    cy.get('.plate').should('have.length', 0)
+  })
+})
