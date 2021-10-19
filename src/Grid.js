1461162658bf687d55b495ab6f4bc37bf02dc7ce
James Glover <james.glover@sanger.ac.uk>
2021-10-19 16:28:11 +0100

    Documentation

--- src/Grid.js
+++ src/Grid.js
@@ -8,8 +8,24 @@
  * The only stipulation is that it needs a row and column - advantage is that it could support any type of object.
  */
 
+/**
+ * Return the row name (eg. 'A') for the given rwo index (eg. 1)
+ * Note: Begins at row 1, not 0
+ *
+ * @param {number} index - The index of the row (eg. 1 for row 'A')
+ * @return {String} The name of the row
+ */
 const rowName = (index) => String.fromCharCode(64 + index)
 
+/**
+ * Return an object describing the rows makeup. Indexed by row name, with
+ * values being objects indexed by column name.
+ *
+ * @param {number} numberOfRows - The number of rows on the plate
+ * @param {Array} columns - Array of column names
+ * @return {Object} Js object with row names as keys, and values being further
+ *                  objects with column names as keys
+ */
 const buildRows = (numberOfRows, columns) => {
   let rows = {}
   for (let i = 1; i <= numberOfRows; i++) {
@@ -19,6 +35,14 @@ const buildRows = (numberOfRows, columns) => {
   return rows
 }
 
+/**
+ * Return an object describing the cow makeup. Indexed by column name, with
+ * values being basic object describing the well.
+ *
+ * @param {String} row - The name of the row
+ * @param {Array} columns - Array of column names
+ * @return {Object} Js object describing the well
+ */
 const buildCells = (row, columns) => {
   const cells = {}
   for (const column of columns) {
@@ -27,6 +51,22 @@ const buildCells = (row, columns) => {
   return cells
 }
 
+/**
+ * @typedef {import('./QuantTypeWellFactories.js').Well} Well
+ */
+
+/**
+ * Return a grid representing the QC plate
+ * and returns the result (eg. 60)
+ *
+ * @param {Object} obj - A configuration object
+ * @param {string} obj.quantType - The name of the process being performed
+ * @param {string} obj.lotNumber - Lot number associated with the QC process
+ * @param {number} obj.numberOfColumns - The number of columns in the plate
+ * @param {number} obj.numberOfRows - The number of rows in the plate
+ * @param {Array} cells - Array of {@link Well}objects to populate the plate
+ * @return {Object} Grid object representing the plate and its contents
+ */
 const grid = (
   {
     quantType = 'libraryPlateReader',
