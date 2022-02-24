8d674a7dcda6c49b9cb6eff05604559a6daa1ca6
Stephen <519327+stevieing@users.noreply.github.com>
2022-02-24 16:57:42 +0000

    Added parsing of filename to extract barcode.

--- tests/e2e/specs/upload_heron_qpcr_file.js
+++ tests/e2e/specs/upload_heron_qpcr_file.js
@@ -7,12 +7,12 @@ describe('heron qPCR', () => {
       .attachFile('HT_132817_2897_2945_2956_HERON_16_2_22_results.csv')
     cy.get('select[id="quant-type"]').select('Heron - qPCR')
     cy.contains('button', 'Upload').click()
-    cy.get('.row > h3').contains('132817')
+    cy.get('.row > h3').contains('HT-132817')
     cy.get('td').should('have.length', 384)
 
     cy.visit('#/plates')
     cy.contains('132817').click()
-    cy.get('.row > h3').contains('132817')
+    cy.get('.row > h3').contains('HT-132817')
 
     // TODO: add post request check after decisions have been made on how to calculate cv
   })
