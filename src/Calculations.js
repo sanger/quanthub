cda35403004dd8d948b22518639b24513ce6ec96
Stuart McHattie <sm49@sanger.ac.uk>
2023-01-26 10:31:13 +0000

    Use camel case!

--- src/Calculations.js
+++ src/Calculations.js
@@ -1,6 +1,6 @@
 import { abs, evaluate, sqrt } from 'mathjs'
 
-let original_value = 'ORIGINAL_VALUE'
+let originalValue = 'ORIGINAL_VALUE'
 
 Number.prototype.toDecimalPlaces = function (n = 3) {
   return Number(this.toFixed(n))
@@ -49,9 +49,9 @@ const isOutlier = (value, limit = 3.5) => {
 // will return 0 if array is empty
 const mean = (
   values,
-  { sample = 0, conversionExpression = `(${original_value})` } = {}
+  { sample = 0, conversionExpression = `(${originalValue})` } = {}
 ) => {
-  if (conversionExpression.indexOf(original_value) === -1) {
+  if (conversionExpression.indexOf(originalValue) === -1) {
     return NaN
   }
 
@@ -60,7 +60,7 @@ const mean = (
   }, 0)
   let mean = sum / (values.length - sample) || 0
 
-  return evaluate(conversionExpression.replaceAll(original_value, mean))
+  return evaluate(conversionExpression.replaceAll(originalValue, mean))
 }
 
 const standardDeviation = (values) => {
