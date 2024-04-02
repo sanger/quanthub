3b61b803573b0b378e96bfb4e46d0c90630bdfcc
Dasun Pubudumal <pubudumald@gmail.com>
2024-04-02 14:51:07 +0100

    Integration tests added

--- tests/e2e/specs/upload_tapestation_file.js
+++ tests/e2e/specs/upload_tapestation_file.js
@@ -1,5 +1,5 @@
 describe('tapestation', () => {
-  it('upload file, check plate is stored', () => {
+  it('upload heron tapestation file, check plate is stored', () => {
     cy.visit('/')
     cy.get('input[type="file"]')
       .as('fileInput')
@@ -26,6 +26,34 @@ describe('tapestation', () => {
     cy.contains('button', 'Export').click()
 
     cy.fixture('tapestationRequest').then((data) => {
+      cy.wait('@postPayload').its('request.body').should('deep.equal', data)
+    })
+  })
+
+  it('upload scRNA tapestation file, check plate is stored', () => {
+    cy.visit('/')
+    cy.get('input[type="file"]')
+      .as('fileInput')
+      .attachFile(
+        '10X_UAT_quantHub_TS_test_2024-03-26 - 13-35-00.csv',
+      )
+    cy.get('select[id="quant-type"]').select('scRNA TapeStation Tubes')
+    cy.contains('button', 'Upload').click()
+    cy.contains('240326-133500-')
+    cy.get('td').should('have.length', 96)
+
+    cy.visit('#/plates')
+    cy.contains('240326-133500-').click()
+    cy.get('h3').contains('240326-133500-')
+
+    cy.intercept('POST', '**/qc_assays', {
+      statusCode: 201,
+      body: {},
+    }).as('postPayload')
+
+    cy.contains('button', 'Export').click()
+
+    cy.fixture('scRNATapeStationRequest').then((data) => {
       cy.wait('@postPayload').its('request.body').should('deep.equal', data)
     })
   })
