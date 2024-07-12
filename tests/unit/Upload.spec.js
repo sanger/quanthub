4c441471b00f0612d9672a4eb14302276f163f31
Ben Topping <ben.topping@outlook.com>
2024-07-12 11:17:05 +0100

    tests(upload): adds some basic tests to upload

--- tests/unit/Upload.spec.js
+++ tests/unit/Upload.spec.js
@@ -1,4 +1,5 @@
 import Upload from '@/components/Upload.vue'
+import quantTypes from '@/config/quantTypes'
 import { beforeEach, describe, expect, it } from 'vitest'
 
 import { mount } from './testHelper'
@@ -13,5 +14,37 @@ describe('Upload.vue', () => {
 
   it('has a message', () => {
     expect(upload.msg).toEqual('Upload')
+  })
+
+  it('shows the correct elements', () => {
+    expect(wrapper.find('form').exists()).toBe(true)
+    // Quant type select is visible
+    expect(wrapper.find('#quant-type').exists()).toBe(true)
+    // File input is hidden
+    expect(wrapper.find('#file-input').exists()).toBe(true)
+    expect(wrapper.find('#file-input').isVisible()).toBe(false)
+    // Browse and upload buttons are visible
+    expect(wrapper.find('button[type="button"]').exists()).toBe(true)
+    expect(wrapper.find('button[type="submit"]').exists()).toBe(true)
+  })
+
+  describe('quanttype selection', () => {
+    it('shows the correct data', () => {
+      // Defaults to empty
+      expect(wrapper.find('#quant-type').element.value).toEqual('')
+      // Has the correct options
+      const options = wrapper.find('#quant-type').findAll('option')
+      expect(options.length).toEqual(Object.entries(quantTypes).length)
+    })
+
+    it('updates quantType when changed', async () => {
+      const options = wrapper.find('#quant-type').findAll('option')
+      expect(upload.quantType).toEqual(null)
+      await options.at(5).setSelected()
+
+      // Check that it updates the upload data
+      const selectedQuantType = Object.entries(quantTypes)[5][0]
+      expect(upload.quantType).toEqual(selectedQuantType)
+    })
   })
 })
