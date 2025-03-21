b6a626e404d8aa1bcc02d05c44e53eb0a4eb0744
Dasun Pubudumal <pubudumald@gmail.com>
2025-03-21 09:26:40 +0000

    Refactor PostCSS and Vite configuration to use new Tailwind CSS packages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import vue from '@vitejs/plugin-vue'
 import { URL, fileURLToPath } from 'url'
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
