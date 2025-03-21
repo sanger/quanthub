0edeb15ceda6fcd31aad17d95ddc97c79203a7a3
Dasun Pubudumal <pubudumald@gmail.com>
2025-03-21 10:20:10 +0000

    Test upgrade - v1

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
-import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import { URL, fileURLToPath } from 'url'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
