3c016c386a429fecf012d5bb638508bbf42c6cc2
Dasun Pubudumal <pubudumald@gmail.com>
2025-03-21 09:28:57 +0000

    Prettify

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -3,4 +3,4 @@ module.exports = {
     require('@tailwindcss/postcss'), // Use the new package
     require('autoprefixer'),
   ],
-};
+}
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
+import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import { URL, fileURLToPath } from 'url'
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
